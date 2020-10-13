217977d39e5bc1e598b643655092c625b27dc500
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-10-13 16:02:53 +0200

    Added Game "Zatopic portal"

---
+++ Rozdzial5Gra/src/main/java/ProstyPortal.java
@@ -0,0 +1,28 @@
+public class ProstyPortal {
+    int [] polaPolozenia;
+    int iloscTrafien;
+
+    public void setPolaPolozenia(int[] ppol) {
+        polaPolozenia = ppol;
+    }
+
+    public String sprawdz(String stringPole){
+        int strzal =Integer.parseInt(stringPole);
+        String wynik = "pudlo";
+
+        for(int pole : polaPolozenia){
+            if(strzal == pole){
+                wynik = "trafiony";
+                iloscTrafien++;
+                break;
+            }
+        }
+
+        if(iloscTrafien == polaPolozenia.length){
+            wynik = "zatopiony";
+        }
+
+        System.out.println(wynik);
+        return wynik;
+    }
+}
